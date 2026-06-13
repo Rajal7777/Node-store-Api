ccc06915bec3be267924b53aaf1915643225a542
Rajal7777 <suwalrajal57@gmail.com>
2026-06-13 15:36:10 +0900

    added routes

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const connectDB = require('./db/connect');
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
@@ -14,6 +15,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send('<h1>Store</h1><a href="/api/v1/products">products list </a>');
 });
+app.use("/api/v1/products", productsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
@@ -23,7 +25,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     //connect db
-    await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI);
     await app.listen(port, console.log(`app is listening at port ${port}`));
   } catch (error) {
     console.log(error);
