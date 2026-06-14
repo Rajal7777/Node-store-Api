2fe7a1c4cfdcbd336dca8a3ddf583f9e14b26a20
Rajal7777 <suwalrajal57@gmail.com>
2026-06-14 13:56:26 +0900

    field property

--- controllers/products.js
+++ controllers/products.js
@@ -3,42 +3,49 @@ const Product = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
   //   throw new Error("testing async errors");
   // const products = await Product.find({});
+  //$options: 'i' mean case sensitive mean iphone !== Iphone
+  const products = await Product.find({}).select("name price");
 
-  const search = 'ab';
-  const products = await Product.find({
-    name: { $regex: search, $options: 'i'} //$options: 'i' mean case sensitive mean iphone !== Iphone
-  });
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
   // console.log(req.query)
 
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
 
-  if(company){
-    queryObject.company = company
+  if (company) {
+    queryObject.company = company;
   }
 
-  if( name ) {
-    queryObject.name =  { $regex: name, $options: 'i'}
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
   }
 
-let result = Product.find(queryObject);
-if(sort){
-  const sortList = sort.split(',').join(' ')
-  result = sort(sortList)
-}else{
-  result = result.sort('createAt')
-}
+  let result = Product.find(queryObject);
 
+  //sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createAt");
+  }
+
+  //field
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+ 
+  console.log(result.fields)
   const products = await result;
-  res.status(200).json({ products,nbHits: products.length  });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
