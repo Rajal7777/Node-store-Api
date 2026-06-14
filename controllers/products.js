3863ec239684dc295ec727fe37a8dced0f010303
Rajal7777 <suwalrajal57@gmail.com>
2026-06-14 12:30:17 +0900

    used of

--- controllers/products.js
+++ controllers/products.js
@@ -3,8 +3,10 @@ const Product = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
   //   throw new Error("testing async errors");
   // const products = await Product.find({});
+
+  const search = 'ab';
   const products = await Product.find({
-    featured: true,
+    name: { $regex: search, $options: 'i'} //$options: 'i' mean case sensitive mean iphone !== Iphone
   });
   res.status(200).json({ products, nbHits: products.length });
 };
@@ -12,7 +14,7 @@ const getAllProductsStatic = async (req, res) => {
 const getAllProducts = async (req, res) => {
   // console.log(req.query)
 
-  const { featured, company } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -21,6 +23,10 @@ const getAllProducts = async (req, res) => {
 
   if(company){
     queryObject.company = company
+  }
+
+  if( name ) {
+    queryObject.name =  { $regex: name, $options: 'i'}
   }
   console.log(queryObject);
   const products = await Product.find(queryObject);
