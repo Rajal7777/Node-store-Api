2439244b385fe92c0e4479d1dd060c5f11ef56ff
Rajal7777 <suwalrajal57@gmail.com>
2026-06-14 12:09:13 +0900

    dynamic query

--- controllers/products.js
+++ controllers/products.js
@@ -10,9 +10,21 @@ const getAllProductsStatic = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
-  console.log(req.query)
-  res.status(200).json({ products });
+  // console.log(req.query)
+
+  const { featured, company } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if(company){
+    queryObject.company = company
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products,nbHits: products.length  });
 };
 
 module.exports = {
