1f4e8b1732dd449df4c8432a52a5844162771e9d
Rajal7777 <suwalrajal57@gmail.com>
2026-06-14 14:11:00 +0900

    pagination

--- controllers/products.js
+++ controllers/products.js
@@ -42,8 +42,16 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
- 
-  console.log(result.fields)
+
+  //pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;  //This formula determines how many records should be ignored before returning results.
+
+  //skip = (2 - 1) * 10 = 10
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
