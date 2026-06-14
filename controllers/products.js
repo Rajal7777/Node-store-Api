4daaeb95cb54f52e0f08470c83960ab8e368b213
Rajal7777 <suwalrajal57@gmail.com>
2026-06-14 11:49:49 +0900

    get the products req.query

--- controllers/products.js
+++ controllers/products.js
@@ -6,12 +6,13 @@ const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({
     featured: true,
   });
-  res.status(200).json({ products,   nbHits: products.length
-   });
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "all products" });
+  const products = await Product.find(req.query);
+  console.log(req.query)
+  res.status(200).json({ products });
 };
 
 module.exports = {
